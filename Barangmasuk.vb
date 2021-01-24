16cc8d314950796895c3b040d93673c908b3985f
Achmad1702 <77906853+Achmad1702@users.noreply.github.com>
2021-01-25 04:40:40 +0700

    Update Barangmasuk.vb

--- Barangmasuk.vb
+++ Barangmasuk.vb
@@ -6,15 +6,19 @@ Public Class Barangmasuk
     Private _kode_supplier As String
     Private _tanggal As String
     Private _nama_penerima As String
+    Private _idmd As Integer
+    Private _kode_barang As Integer
+    Private _nama_barang As String
+    Private _qty As String
     Public InsertState As Boolean = False
     Public UpdateState As Boolean = False
     Public DeleteState As Boolean = False
-    Public Property idamasuk()
+    Public Property idmd()
         Get
-            Return _idamasuk
+            Return _idmd
         End Get
         Set(ByVal value)
-            _idamasuk = value
+            _idmd = value
         End Set
     End Property
     Public Property nomor_faktur()
@@ -25,6 +29,24 @@ Public Class Barangmasuk
             _nomor_faktur = value
         End Set
     End Property
+
+    Public Property qty()
+        Get
+            Return _qty
+        End Get
+        Set(ByVal value)
+            _qty = value
+        End Set
+    End Property
+    Public Property idamasuk()
+        Get
+            Return _idamasuk
+        End Get
+        Set(ByVal value)
+            _idamasuk = value
+        End Set
+    End Property
+ 
     Public Property kode_supplier()
         Get
             Return _kode_supplier
@@ -49,6 +71,24 @@ Public Class Barangmasuk
             _nama_penerima = value
         End Set
     End Property
+   
+    Public Property kode_barang()
+        Get
+            Return _kode_barang
+        End Get
+        Set(ByVal value)
+            _kode_barang = value
+        End Set
+    End Property
+    Public Property nama_barang()
+        Get
+            Return _nama_barang
+        End Get
+        Set(ByVal value)
+            _nama_barang = value
+        End Set
+    End Property
+    
     Public Sub Simpan()
         Dim info As String
         DBConnect()
@@ -136,4 +176,92 @@ Public Class Barangmasuk
             DBDisconnect()
         End Try
     End Sub
+    Public Sub SimpanDetail()
+        DBConnect()
+        If (bmdetail_baru = True) Then
+            strsql = "Insert into bmdetail(idmd,nomor_faktur,kode_barang,nama_barang,qty) values ('" & _idmd & "','" & _nomor_faktur & "','" & _kode_barang & "','" & _nama_barang & "','" & _qty & "')"
+            info = "INSERT"
+        Else
+            strsql = "update bmdetail set idmd='" & _idmd & "', nomor_faktur='" & _nomor_faktur & "', kode_barang='" & _kode_barang & "', nama_barang='" & _nama_barang & "', qty='" & _qty & "' where nomor_faktur='" & _nomor_faktur & "'"
+            info = "UPDATE"
+        End If
+        myCommand.Connection = conn
+        myCommand.CommandText = strsql
+        Try
+            myCommand.ExecuteNonQuery()
+        Catch ex As Exception
+            If (info = "INSERT") Then
+                InsertState = False
+            ElseIf (info = "UPDATE") Then
+                UpdateState = False
+            Else
+            End If
+        Finally
+            If (info = "INSERT") Then
+                InsertState = True
+            ElseIf (info = "UPDATE") Then
+                UpdateState = True
+            Else
+            End If
+        End Try
+        DBDisconnect()
+    End Sub
+    Public Sub Caribmdetail(ByVal sidmd As String)
+        DBConnect()
+        strsql = "SELECT * FROM bmdetail WHERE idmd='" & sidmd & "'"
+        myCommand.Connection = conn
+        myCommand.CommandText = strsql
+        DR = myCommand.ExecuteReader
+        If (DR.HasRows = True) Then
+            bmdetail_baru = False
+            DR.Read()
+            idmd = Convert.ToString((DR("idmd")))
+            nomor_faktur = Convert.ToString((DR("nomor_faktur")))
+            kode_barang = Convert.ToString((DR("kode_barang")))
+            nama_barang = Convert.ToString((DR("nama_barang")))
+            qty = Convert.ToString((DR("qty")))
+        Else
+            MessageBox.Show("Data Tidak Ditemukan.")
+            bmdetail_baru = True
+        End If
+        DBDisconnect()
+    End Sub
+    Public Sub HapusDetail(ByVal sidmd As String)
+        Dim info As String
+        DBConnect()
+        strsql = "DELETE FROM bmdetail WHERE idmd='" & sidmd & "'"
+        info = "DELETE"
+        myCommand.Connection = conn
+        myCommand.CommandText = strsql
+        Try
+            myCommand.ExecuteNonQuery()
+            DeleteState = True
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
+        DBDisconnect()
+    End Sub
+    Public Sub getAllDataDetail(ByVal dg As DataGridView)
+        Try
+            DBConnect()
+            strsql = "SELECT * FROM bmdetail"
+            myCommand.Connection = conn
+            myCommand.CommandText = strsql
+            myData.Clear()
+            myAdapter.SelectCommand = myCommand
+            myAdapter.Fill(myData)
+            With dg
+                .DataSource = myData
+                .AllowUserToAddRows = False
+                .AllowUserToDeleteRows = False
+                .ReadOnly = True
+            End With
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        Finally
+            DBDisconnect()
+        End Try
+    End Sub
 End Class
+
+
